fef22d25eaedcca80b51507e63327187dd79464b
Gaurav-Codes-17 <ssood463@gmail.com>
2026-02-26 12:41:00 +0530

    rate limiting

--- app/api/track/route.js
+++ app/api/track/route.js
@@ -1,19 +1,19 @@
 import { NextResponse } from "next/server";
 
 const visitMap = new Map();
-const COOLDOWN_MS = 60 * 60 * 1000; 
+const COOLDOWN_MS = 5 * 60 * 1000; 
 
 export async function POST(req) {
   try {
   const rawIp = req.headers.get("x-forwarded-for")?.split(",")[0].trim();
 const ip = (!rawIp || rawIp === "::1" || rawIp === "127.0.0.1") ? "8.8.8.8" : rawIp;
 
-    // // Rate limit check
-    // const lastVisit = visitMap.get(ip);
-    // if (lastVisit && Date.now() - lastVisit < COOLDOWN_MS) {
-    //   console.log("⏳ Rate limited:", ip);
-    //   return NextResponse.json({ success: false, reason: "rate_limited" });
-    // }
+    // Rate limit check
+    const lastVisit = visitMap.get(ip);
+    if (lastVisit && Date.now() - lastVisit < COOLDOWN_MS) {
+      console.log("⏳ Rate limited:", ip);
+      return NextResponse.json({ success: false, reason: "rate_limited" });
+    }
     visitMap.set(ip, Date.now());
 
     // switched to ip-api.com — works better server-side
